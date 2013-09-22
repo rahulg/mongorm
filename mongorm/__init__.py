86124196a5041ad5735b716d14e84855af0f3c02
Rahul AG <r@hul.ag>
2013-09-23 01:35:04 +0800

    Version bump: 0.4.1.

--- mongorm/__init__.py
+++ mongorm/__init__.py
@@ -4,6 +4,6 @@ from mongorm.database import Database
 from mongorm.document import Field
 from mongorm.utils import DotDict
 
-VERSION = (0, 4, 0)
+VERSION = (0, 4, 1)
 
 __all__ = ['VERSION', 'Database', 'Field', 'DotDict']
