1af85da8e08078f959a988c9435b37058781d0b6
Rahul AG <r@hul.ag>
2013-09-25 17:18:24 +0800

    Version bump: 0.6.4.

--- mongorm/__init__.py
+++ mongorm/__init__.py
@@ -4,7 +4,7 @@ from mongorm.database import Database
 from mongorm.document import Field, Index
 from mongorm.utils import DotDict, JSONEncoder
 
-VERSION = (0, 6, 3)
+VERSION = (0, 6, 4)
 
 
 class ValidationError(Exception):
