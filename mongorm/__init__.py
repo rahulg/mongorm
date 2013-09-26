72c7f012e5cff96a603dcdc342c0b6f5c7156f36
Rahul AG <r@hul.ag>
2013-09-26 18:06:41 +0800

    Version bump: 0.6.7.

--- mongorm/__init__.py
+++ mongorm/__init__.py
@@ -4,8 +4,6 @@ from mongorm.database import Database
 from mongorm.document import Field, Index
 from mongorm.utils import DotDict, JSONEncoder
 
-VERSION = (0, 6, 6)
-
 
 class ValidationError(Exception):
     pass
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 import os
 from setuptools import setup
 
-VERSION = (0, 6, 6)
+VERSION = (0, 6, 7)
 
 
 def read(fname):
